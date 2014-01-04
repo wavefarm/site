f243b5fc840db0e4b609324165c826a180e19ebe
Gabriel Farrell <g@grrawr.com>
2014-01-04 01:02:39 -0500

    Browserified tweet JS. Refs #33

---
+++ browser/main.js
@@ -0,0 +1,4 @@
+var tweets = require('./tweets');
+
+
+tweets();
---
+++ browser/tweets.js
@@ -0,0 +1,37 @@
+var fade = require('fade');
+var concat = require('concat-stream');
+var http = require('http')
+
+
+module.exports = function () {
+  http.get({path: '/tweets'}, function (res) {
+    res.pipe(concat(function (data) {
+      var i, a, t, current, next, tweetsDiv, tweets = JSON.parse(data);
+      // Append to .tweets
+      tweetsDiv = document.getElementsByClassName('tweets')[0];
+      for (i=0; i<tweets.length; i++) {
+        t = tweets[i];
+        a = document.createElement('a');
+        a.setAttribute('class', 'tweet');
+        a.setAttribute('target', '_blank');
+        a.setAttribute('href', '//twitter.com/free103point9/status/' + t.id);
+        a.innerHTML = t.text;
+        tweetsDiv.appendChild(a);
+      }
+      // Cycle through them, displaying one at a time
+      tweets = tweetsDiv.getElementsByClassName('tweet');
+      current = tweets[i=0];
+      current.style.display = 'inline';
+      setInterval(function () {
+        if (++i >= tweets.length) i = 0;
+        next = tweets[i];
+        fade.out(current, function () {
+          current.style.display = 'none';
+          fade.in(current); // fade it back in for the next cycle
+          next.style.display = 'inline';
+          current = next;
+        });
+      }, 8000);
+    }));
+  });
+};
