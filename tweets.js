570398300a687ae477c113ac6e4a030fe716aac4
Gabriel Farrell <g@grrawr.com>
2014-01-25 19:35:03 -0500

    Twit err.message only

--- tweets.js
+++ tweets.js
@@ -28,7 +28,7 @@ module.exports = function (req, res) {
     trim_user: true
   }, function (err, reply) {
     if (err) {
-      console.error(err.stack);
+      console.error(err.message);
       res.statusCode = 500;
       return res.end(JSON.stringify({
         error: err.message
