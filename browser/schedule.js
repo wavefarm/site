7ee6f723b1ad1bf0b9c4147382ee4b26d77620e2
Gabriel Farrell <g@grrawr.com>
2014-01-26 00:32:33 -0500

    Show a week of days on the schedule. Refs #55

--- browser/schedule.js
+++ browser/schedule.js
@@ -8,24 +8,45 @@ module.exports = function () {
   var schedule = $('.schedule');
   if (!schedule.length) return;
 
-  var now = moment();
-  http.get({
-    path: '/api/wgxc/schedule/' + now.format('YYYY-MM-DD')
-  }, function (res) {
-    res.pipe(concat(function (data) {
-      var result = JSON.parse(data);
-      var day = $('.day');
-      day.find('.month').html(now.format('MMMM'));
-      day.find('.monthday').html(now.format('DD'));
-      var showList = day.find('.show-list');
-      var showTemplate = day.find('.show').detach().clone();
-      result.hits.forEach(function (hit) {
-        var show = showTemplate.clone();
-        var start = moment(hit.start);
-        show.find('.show-time').html(start.format('h:mma'));
-        show.find('.show-name').html(hit.name);
-        showList.append(show);
-      });
-    }));
-  });
+  // Fill in existing .day with today's shows, but also use it
+  // as a template for future days.
+  var date = moment();
+  var day = $('.day');
+  var dayTemplate = day.clone();
+  var showTemplate = day.find('.show').detach().clone();
+  var daysToGet = 7;
+  var today = true;
+
+  function renderDay () {
+    http.get({
+      path: '/api/wgxc/schedule/' + date.format('YYYY-MM-DD')
+    }, function (res) {
+      res.pipe(concat(function (data) {
+        var result = JSON.parse(data);
+        console.log(result)
+        if (today) {
+          today = false;
+        } else {
+          day = dayTemplate.clone();
+          schedule.append(day);
+          day.find('.weekday').html(date.format('dddd'));
+        }
+        day.find('.month').html(date.format('MMMM'));
+        day.find('.monthday').html(date.format('DD'));
+        var showList = day.find('.show-list');
+        result.hits.forEach(function (hit) {
+          var show = showTemplate.clone();
+          show.find('.show-time').html(moment(hit.start).format('h:mma'));
+          show.find('.show-name').html(hit.name);
+          showList.append(show);
+        });
+        if (--daysToGet) {
+          date = date.add('days', 1);
+          renderDay();
+        }
+      }));
+    });
+  }
+
+  renderDay();
 };
