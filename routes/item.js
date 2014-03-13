9abf20f6cdc68eec96e500cab733b84ab2df0dae
Gabriel Farrell <g@grrawr.com>
2014-03-13 00:13:35 -0400

    Switch item route to hyperstream. Refs #76

--- routes/item.js
+++ routes/item.js
@@ -1,30 +1,11 @@
-var cheerio = require('cheerio')
-, fs = require('fs')
-
-var templates = {}
-
-var loadTemplate = function (name, cb) {
-  fs.readFile(__dirname + '/../templates/' + name + '.html', {encoding: 'utf8'}, function (err, data) {
-    if (err) console.error(err)
-    templates[name] = data
-    cb()
-  })
-}
+var hyperstream = require('hyperstream')
+, t = require('../templates')
 
 module.exports = function (req, res) {
-  var pending = 3
-  , sendRes = function () {
-    if (--pending) return
-    var $ = cheerio.load(templates.item)
-    , $layout = cheerio.load(templates.layout)
-    $('.item-title').text('Item title')
-    $layout('.head').html(templates.head)
-    $layout('.main').html($.html())
-    res.setHeader('Content-Type', 'text/html');
-    res.end($layout.html())
-  }
-  loadTemplate('layout', sendRes)
-  loadTemplate('item', sendRes)
-  loadTemplate('head', sendRes)
+  t('/layout.html').pipe(hyperstream(
+  { '.head': t('/head.html')
+  , '.main': t('/item.html').pipe(hyperstream(
+    { '.item-title': 'Item title'
+    }))
+  })).pipe(res)
 }
-
