89d576c2c8f02ed4498a8771cc586f833fe4786b
Gabriel Farrell <g@grrawr.com>
2014-06-16 10:53:35 -0400

    Cleanup

--- browser/subnav.js
+++ browser/subnav.js
@@ -1,6 +1,5 @@
 module.exports = function () {
   $('.primary.wgxc a').hover(function () {
-    console.log(this.pathname);
     $('.secondary.wgxc a[href="'+this.pathname+'"]').toggle();
   });
 };
