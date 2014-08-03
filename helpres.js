03cb3a9be69b8c1b1b14ef6fba216ad4a8e361dc
Gabriel Farrell <g@grrawr.com>
2014-08-03 00:31:27 -0400

    Just warn with message on 404

--- helpres.js
+++ helpres.js
@@ -1,17 +1,20 @@
-var http = require('http');
+var http = require('http')
 
 
 module.exports = function helpres (res) {
   res.error = function (code, err) {
-    console.error(err.stack);
-    res.statusCode = code;
-    return res.end(http.STATUS_CODES[code]);
-  };
+    if (code == 404) console.warn('Warning:', err.message)
+    else console.error(err.stack)
+
+    res.statusCode = code
+    return res.end(http.STATUS_CODES[code])
+  }
 
   res.redirect = function (to, code) {
-    console.warn('Warning: Moved Permanently');
-    res.statusCode = code || 301;
-    res.setHeader('location', to);
-    res.end('Moved Permanently');
-  };
-};
+    console.warn('Warning: Moved Permanently')
+
+    res.statusCode = code || 301
+    res.setHeader('location', to)
+    res.end('Moved Permanently')
+  }
+}
