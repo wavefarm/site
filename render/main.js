4a62bce761129b2f86cb9fd3087fefe4fda7909e
Gabriel Farrell <g@grrawr.com>
2014-08-11 16:22:59 -0400

    Archive renderer at archive/index

--- render/main.js
+++ render/main.js
@@ -4,7 +4,7 @@ var h = require('virtual-hyperscript')
 module.exports = function (state) {
   // "Routing" for main depending on state
   if (state.archive) {
-    return require('./archive/main')(state.archive)
+    return require('./archive')(state.archive)
   }
 
   // TODO Render site index page if nothing found in state
