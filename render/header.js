f5bbe84eee98b88e56cfafdcca32f4e008e1c3aa
Gabriel Farrell <g@grrawr.com>
2014-08-13 11:32:11 -0400

    Header search input is type=search

--- render/header.js
+++ render/header.js
@@ -4,7 +4,7 @@ var h = require('virtual-hyperscript')
 module.exports = function (state) {
   return h('.header-fill', h('header',
     h('.search', h('form', {action: '/archive'},
-      h('input#q', {type: 'text', name: 'q', placeholder: 'SEARCH'})
+      h('input#q', {type: 'search', name: 'q', placeholder: 'SEARCH'})
     ))
   ))
 }
