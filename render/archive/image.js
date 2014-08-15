921b1b2c89131bf2a061cb1a04775269a41e5f5a
Gabriel Farrell <g@grrawr.com>
2014-08-14 20:39:29 -0400

    Display image for image items. Fix #108

--- render/archive/image.js
+++ render/archive/image.js
@@ -1,4 +1,5 @@
 var h = require('virtual-hyperscript')
+var moment = require('moment')
 
 module.exports = function (item) {
   return h('.item#' + item.id, [
@@ -8,6 +9,10 @@ module.exports = function (item) {
       h('span.item-type', '(' + item.type + ')')
     ]),
     h('.caption', item.caption),
+    h('.date', h('time', {dateTime: item.date},
+      moment(item.date).format('ll')
+    )),
+    h('.image', h('img', {alt: item.main, src: item.url})),
     h('.download',
       h('a', {href: item.url, download: true}, 'Download (' + item.mimetype + ')')
     )
