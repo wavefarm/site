d8af2243a9795c025dce518501fdae10e8e4083e
Gabriel Farrell <g@grrawr.com>
2014-08-30 00:44:39 -0400

    Site icons in archive results. #106

--- render/archive/link.js
+++ render/archive/link.js
@@ -3,8 +3,25 @@ var itemClick = require('../../events/item-click')
 var renderDate = require('../date')
 
 
+function getSites (item) {
+  var sites = []
+  function pushSite (site) {
+    if (sites.indexOf(site) == -1) sites.push(site)
+  }
+  if (item.sites) {
+    if (item.sites.indexOf('transmissionarts.org') != -1) pushSite('ta')
+    if (item.sites.indexOf('wgxc.org') != -1) pushSite('wgxc')
+  }
+  if (item.type == 'broadcast') pushSite('wgxc')
+  if (item.type == 'show') pushSite('wgxc')
+  return sites.map(function (site) {
+    return h('.' + site + '.site-icon')
+  })
+}
+
 module.exports = function (item) {
-  return h('.item',
+  return h('.item', [
+    h('.item-subsites', getSites(item)),
     h('a.item-link#' + item.id, {
       'href': '/archive/' + item.id,
       'ev-click': itemClick
@@ -16,5 +33,5 @@ module.exports = function (item) {
       ]),
       h('.date', renderDate(item))
     ])
-  )
+  ])
 }
