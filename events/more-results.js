c81897768a10a0175920f58edc45a0efbb88620e
Gabriel Farrell <g@grrawr.com>
2014-10-07 00:57:35 -0400

    Fix more button

--- events/more-results.js
+++ events/more-results.js
@@ -4,10 +4,10 @@ var qs = require('querystring')
 
 module.exports = function () {
   var state = window.state
-  var params = {
-    q: state.archive.q(),
-    size: state.archive.results().hits.length + 10
-  }
+
+  var params = state.archive.params()
+  params.size = state.archive.results().hits.length + 10
+
   api.search(params, function (err, results) {
     if (err) return console.error(err)
     state.archive.results.set(results)
