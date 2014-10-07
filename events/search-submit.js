efd00dcda21966e64cc700ff056b27dfe84f917b
Gabriel Farrell <g@grrawr.com>
2014-10-07 00:58:10 -0400

    Simpler params handling

--- events/search-submit.js
+++ events/search-submit.js
@@ -6,11 +6,9 @@ var ve = require('value-event/value')
 module.exports = ve(function (params) {
   var state = window.state
 
-  var q = params.q
-  var date = params.date
-  var date2 = params.date2
   state.archive.params.set(params)
-  state.title.set('Search for ' + q)
+  state.title.set('Search' + (params.q ? ' for ' + params.q : ''))
+
   api.search(params, function (err, results) {
     if (err) return console.error(err)
     state.archive.item.set(null)
