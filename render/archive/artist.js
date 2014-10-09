b22c5f6779e3fee2f80161b389de96d22bf0bb21
Gabriel Farrell <g@gsf.io>
2014-10-09 00:20:47 -0400

    Use vdomify in artist item. #96

--- render/archive/artist.js
+++ render/archive/artist.js
@@ -1,5 +1,5 @@
 var h = require('virtual-hyperscript')
-var fromHTML = require('vdom-virtualize').fromHTML
+var vdomify = require('vdomify')
 
 module.exports = function (item) {
   return h('.item#' + item.id, [
@@ -8,9 +8,6 @@ module.exports = function (item) {
       ' ',
       h('span.item-type', '(' + item.type + ')')
     ]),
-    // vdom-virtualize breaks everything when run on the server
-    (typeof window !== 'undefined' ?
-      fromHTML('<div class="bio">'+item.bio+'</div>') :
-      h('.bio', item.bio))
+    vdomify('.bio', item.bio)
   ])
 }
