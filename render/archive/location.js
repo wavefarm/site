e2a62df9decf0e4bb166b0368f3e68da238a74d6
Gabriel Farrell <g@gsf.io>
2014-10-09 03:33:27 -0400

    Fill in location item. Fix #126

--- render/archive/location.js
+++ render/archive/location.js
@@ -1,4 +1,18 @@
 var h = require('virtual-hyperscript')
+var vdomify = require('vdomify')
+
+
+function concoctFullAddress (item) {
+  var addy = ''
+  addy += item.address ? item.address + '<br>' : ''
+  addy += item.address2 ? item.address2 + '<br>' : ''
+  addy += item.city || ''
+  addy += item.city && item.state ? ', ' : ''
+  addy += item.state ? item.state + ' ' : ''
+  addy += item.postalCode ? item.postalCode + '<br>' : ''
+  addy += item.country || ''
+  return addy
+}
 
 module.exports = function (item) {
   return h('.item#' + item.id, [
@@ -7,9 +21,8 @@ module.exports = function (item) {
       ' ',
       h('span.item-type', '(' + item.type + ')')
     ]),
-    h('.caption', item.caption),
-    h('.download',
-      h('a', {href: item.url, download: true}, 'Download (' + item.mimetype + ')')
-    )
+    vdomify('.address', concoctFullAddress(item)),
+    h('.phone', item.phone),
+    h('.url',  h('a', {_target: 'blank', href: item.url}, item.url))
   ])
 }
