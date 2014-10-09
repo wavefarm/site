a99e71566304fc8fed1157a923fad3a1a309bab2
Gabriel Farrell <g@gsf.io>
2014-10-09 00:29:42 -0400

    Description in broadcast. #127

--- render/archive/broadcast.js
+++ render/archive/broadcast.js
@@ -1,5 +1,7 @@
 var h = require('virtual-hyperscript')
 var renderDate = require('../date')
+var vdomify = require('vdomify')
+
 
 module.exports = function (item) {
   return h('.item#' + item.id, [
@@ -8,6 +10,7 @@ module.exports = function (item) {
       ' ',
       h('span.item-type', '(' + item.type + ')')
     ]),
-    h('.date', renderDate(item))
+    h('.date', renderDate(item)),
+    vdomify('.description', item.description)
   ])
 }
