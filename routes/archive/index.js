a6eb58cdae645b63a7c4c15a8164b3d4d52da421
Gabriel Farrell <g@gsf.io>
2014-10-16 00:53:11 -0400

    Fewer lines

--- routes/archive/index.js
+++ routes/archive/index.js
@@ -3,10 +3,7 @@ var render = require('../../render/layout')
 
 
 module.exports = function (req, res) {
-  var state = {
-    section: 'archive',
-    title: 'Archive'
-  }
+  var state = {section: 'archive', title: 'Archive'}
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end('<!doctype html>' + String(elemify(render(state))))
 }
