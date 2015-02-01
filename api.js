b5858d466cbffc6639b9a1c69d9a9a1b28ca37bd
Gabriel Farrell <g@gsf.io>
2015-02-01 00:09:59 -0500

    Simplify api setup

--- api.js
+++ api.js
@@ -1,13 +1,6 @@
 var wf = require('wavefarm')
 
-var options = {path: '/api'}
+var options = {}
 
-if (typeof window !== 'undefined') {
-  options.host = window.location.hostname
-  options.port = window.location.port
-} else {
-  options.host = 'localhost'
-  options.port = process.env.PORT || 1041
-}
 
 module.exports = wf(options)
