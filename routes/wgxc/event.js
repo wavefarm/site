1bcd2417de53088c7eb82391cd3a52f1c1a2ab87
Peter Berry <peter@Peters.local>
2015-02-10 17:14:17 -0800

    updated calendar, event and item pages; updated listen live bar

--- routes/wgxc/event.js
+++ routes/wgxc/event.js
@@ -1,6 +1,8 @@
 var api = require('../../api')
 var hs = require('hyperstream')
 var t = require('../../templates')
+var moment = require('moment')
+var wgxc = require('../../lib/wgxc')
 
 var idRe = /\/wgxc\/calendar\/(\w{6})/
 var wgxcTypes = [
@@ -19,6 +21,25 @@ module.exports = function (req, res) {
     if (wgxcTypes.indexOf(item.type) === -1) {
       return res.error(404, new Error('No type "' + item.type + '"'))
     }
+    
+    
+    //var datesDesc = moment(item.startDate).format('MMM D, YYYY') + ' - ' + moment(item.endDate).format('MMM D, YYYY');
+    var datesDesc = wgxc.formatDates(item);
+
+    var locationName = '';
+    var locationAddress = '';
+    
+    if(typeof(item.locations)!='undefined' && item.locations.length>0) {
+        var locationName = item.locations[0].main;    	
+    }
+    
+    var iconList = wgxc.getIconList(item);   
+    var icons = '';
+    for (i=0; i<iconList.length; i++) {
+    	icons += '<img src="' + iconList[i] +'" />';
+    }
+        
+    
 
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     t('/layout.html').pipe(hs({
@@ -26,7 +47,11 @@ module.exports = function (req, res) {
       '.head': t('/wgxc/head.html'),
       '.nav': t('/wgxc/nav.html'),
       '.main': t('/wgxc/event.html').pipe(hs({
-        '.item-main': item.main,
+        '.item-main span.main': item.main,
+        '.item-main span.icons': icons,        
+        '.event-dates strong': datesDesc,
+        '.event-location strong': locationName,
+        //'.event-location p': locationAddress,
         '.description': item.briefDescription,
         '.start-date': item.startDate,
         '.end-date': item.endDate
--- routes/wgxc/item.js
+++ routes/wgxc/item.js
@@ -1,6 +1,7 @@
 var api = require('../../api')
 var hs = require('hyperstream')
 var t = require('../../templates')
+var wgxc = require('../../lib/wgxc')
 
 var idRe = /\/wgxc\/schedule\/(\w{6})/
 var wgxcTypes = [
@@ -19,13 +20,37 @@ module.exports = function (req, res) {
       return res.error(404, new Error('No type "' + item.type + '"'))
     }
 
+    var datesDesc = wgxc.formatDates(item);
+    
+    var main = item.main;
+    var show = '';
+    
+    if (item.type=='broadcast' && typeof(item.shows)!='undefined') {
+    	show =  '<a href="/wgxc/schedule/'+item.shows[0].id+'">'+ item.shows[0].main +'</a>:&nbsp;';
+    }
+    
+    var iconList = wgxc.getIconList(item);   
+    var icons = '';
+    for (i=0; i<iconList.length; i++) {
+    	icons += '<img src="' + iconList[i] +'" />';
+    }
+    
+    var detailDesc = '';
+    if (typeof(item.credit)!='undefined')
+    	detailDesc = item.credit;
+    
+    
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     t('/layout.html').pipe(hs({
       title: item.main,
       '.head': t('/wgxc/head.html'),
       '.nav': t('/wgxc/nav.html'),
       '.main': t('/wgxc/item.html').pipe(hs({
-        '.item-main': item.main,
+        '.item-main strong': show,
+        '.item-main span.main': main,
+        '.item-main span.icons': icons,
+        '.item-dates strong': datesDesc,
+        '.item-detail strong': detailDesc,
         '.description': item.description
       }))
     })).pipe(res)
