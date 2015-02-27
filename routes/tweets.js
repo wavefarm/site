63428e38fc169e2da3c70f94b747ae2f9e6ca6f7
Gabriel Farrell <g@gsf.io>
2015-02-27 06:08:27 +0000

    Just skip tweets if not credentials

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,47 +1,60 @@
 var Twit = require('twit')
 
-var twit = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-})
-
-var cache = {
-  tweets: null,
-  updated: null
-}
-
-module.exports = function (req, res) {
-  var now = Date.now()
-  res.setHeader('Content-Type', 'application/json')
-
-  // Serve cache if less than a minute old
-  if (cache.tweets && cache.updated > (now - 60000)) {
-    return res.end(cache.tweets)
+function getTwits () {
+  // noop if no twitter credentials
+  if (!process.env.TWITTER_ACCESS_TOKEN) {
+    return function (req, res) {
+      res.setHeader('Content-Type', 'application/json')
+      res.statusCode = 401
+      res.end('{"message": "Unauthorized"}')
+    }
   }
 
-  twit.get('statuses/user_timeline', {
-    screen_name: 'free103point9',
-    count: 10,
-    trim_user: true
-  }, function (err, reply) {
-    if (err) {
-      console.error(err.message)
-      res.statusCode = 500
-      return res.end(JSON.stringify({
-        error: err.message
-      }))
+  var twit = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+
+  var cache = {
+    tweets: null,
+    updated: null
+  }
+
+  return function (req, res) {
+    var now = Date.now()
+    res.setHeader('Content-Type', 'application/json')
+
+    // Serve cache if less than a minute old
+    if (cache.tweets && cache.updated > (now - 60000)) {
+      return res.end(cache.tweets)
     }
 
-    cache.updated = now
-    cache.tweets = JSON.stringify(reply.map(function (tweet) {
-      return {
-        id: tweet.id_str,
-        timestamp: tweet.created_at,
-        text: tweet.text
+    twit.get('statuses/user_timeline', {
+      screen_name: 'free103point9',
+      count: 10,
+      trim_user: true
+    }, function (err, reply) {
+      if (err) {
+        console.error(err.message)
+        res.statusCode = 500
+        return res.end(JSON.stringify({
+          error: err.message
+        }))
       }
-    }))
-    res.end(cache.tweets)
-  })
+
+      cache.updated = now
+      cache.tweets = JSON.stringify(reply.map(function (tweet) {
+        return {
+          id: tweet.id_str,
+          timestamp: tweet.created_at,
+          text: tweet.text
+        }
+      }))
+      res.end(cache.tweets)
+    })
+  }
 }
+
+module.exports = getTwits()
