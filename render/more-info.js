1d1fb7d49a8e3c434a7b2c631f3d1fd7ea3a820a
Gabriel Farrell <g@gsf.io>
2015-05-26 19:41:23 -0400

    Drop WGXC more-info links. Fix #151

--- render/more-info.js
+++ render/more-info.js
@@ -1,32 +1,17 @@
 var h = require('virtual-hyperscript')
 
-var wgxcTypeMap = {
-  artist: 'people',
-  broadcast: 'events',
-  collaborator: 'people',
-  event: 'events',
-  location: 'places',
-  show: 'schedule',
-  audio: 'archives',
-  video: 'archives',
-  image: 'archives',
-  text: 'archives'
-}
-
 module.exports = function (item) {
   return h('ul.more-info',
     item.sites.map(function (site) {
-      var link
       if (site === 'transmissionarts') {
-        link = 'http://' + site + '.org/' + item.type + '/' + item.id
-      } else if (site === 'wgxc') {
-        if (!wgxcTypeMap[item.type]) link = ''
-        else link = 'http://' + site + '.org/' + wgxcTypeMap[item.type] + '/' + item.oldId
+        return h('li', [
+          'More info at ',
+          h('a', {
+            href: 'http://' + site + '.org/' + item.type + '/' + item.id,
+            target: '_blank'
+          }, site + '.org')
+        ])
       }
-      return h('li', [
-        'More info at ',
-        h('a', {href: link, target: '_blank'}, site + '.org')
-      ])
     })
   )
 }
