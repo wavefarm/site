504e63640110296b8f5875ea148a609fdc149cf3
ptberry <ptberry@gmail.com>
2015-06-01 15:01:16 -0700

    initial commit of /ta/artists, /ta/works/, etc

---
+++ routes/ta/artist.js
@@ -0,0 +1,38 @@
+var api = require('../../api')
+var hs = require('hyperstream')
+var t = require('../../templates')
+var util = require('../../lib/util')
+
+var idRe = /\/ta\/artists\/(\w{6})/
+var validTypes = [
+  'artist'
+]
+
+module.exports = function (req, res) {
+	var matches = idRe.exec(req.url)
+  var id = matches[1]
+  api.get(id, function (err, item) {
+    if (err) {
+      return res.error(500, err)
+    }
+
+    if (validTypes.indexOf(item.type) === -1) {
+      return res.error(404, new Error('No type "' + item.type + '"'))
+    }
+
+    var main = item.main;
+     
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    t('/layout.html').pipe(hs({
+      title: item.main,
+      '.head': t('/ta/head.html'),
+      '.nav': t('/ta/nav.html'),
+      '.listen': t('/listen.html'),      
+      '.main': t('/ta/artist.html').pipe(hs({
+        '.item-main span.main': main,
+        //'.item-main-image': { src: imgSrc },
+        '.description': typeof(item.bio)!='undefined'?item.bio:''
+      }))
+    })).pipe(res)
+  })
+}
---
+++ routes/ta/work.js
@@ -0,0 +1,44 @@
+var api = require('../../api')
+var hs = require('hyperstream')
+var t = require('../../templates')
+var util = require('../../lib/util')
+
+var idRe = /\/ta\/works\/(\w{6})/
+var validTypes = [
+  'work'
+]
+
+module.exports = function (req, res) {
+	var matches = idRe.exec(req.url)
+  var id = matches[1]
+  api.get(id, function (err, item) {
+    if (err) {
+      return res.error(500, err)
+    }
+
+    if (validTypes.indexOf(item.type) === -1) {
+      return res.error(404, new Error('No type "' + item.type + '"'))
+    }
+
+    console.log(item);
+    
+    var main = item.main;
+    var datesDesc = util.formatArchiveDate(item);    
+    var credit = item.credit || '';
+     
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    t('/layout.html').pipe(hs({
+      title: item.main,
+      '.head': t('/ta/head.html'),
+      '.nav': t('/ta/nav.html'),
+      '.listen': t('/listen.html'),      
+      '.main': t('/ta/work.html').pipe(hs({
+        '.item-main span.main': main,
+        //'.item-main-image': { src: imgSrc },
+        '.item-credit strong': credit,                
+        '.item-dates strong': datesDesc,
+        '.description': typeof(item.description)!='undefined'?item.description:'No desc'
+      }))
+    })).pipe(res)
+  })
+}
