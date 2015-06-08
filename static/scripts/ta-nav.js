4987820234aebdd94df30daf4946ba8db4326167
ptberry <ptberry@gmail.com>
2015-06-08 10:04:01 -0700

    Fix #186

--- static/scripts/ta-nav.js
+++ static/scripts/ta-nav.js
@@ -3,9 +3,9 @@
   function current2nd (section) { document.getElementById(section+'-nav').style.color = '#ffcd32'; }
   
   if (window.location.pathname == '/ta/residencies') current('residencies');
-  else if (window.location.pathname == '/ta/schedule') current('schedule');
-  else if (window.location.pathname == '/ta/calendar') current('calendar');
-  else if (window.location.pathname.indexOf('/ta/support')==0) current('support');
+  else if (window.location.pathname == '/ta/support') current('support');
+  else if (window.location.pathname.indexOf('/ta/schedule')==0) current('schedule');
+  else if (window.location.pathname.indexOf('/ta/calendar')==0) current('calendar');
   
   else if (window.location.pathname.indexOf('/ta/archive')==0)	{ 
   	document.getElementById('nav-secondary-ta').style.display = 'block'; 
