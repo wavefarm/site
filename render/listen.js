50201c2af66ae09d689c4880dd8de8e0837529ac
Gabriel Farrell <g@gsf.io>
2015-06-12 07:55:02 -0400

    Cache listen template on module load. #153

--- render/listen.js
+++ render/listen.js
@@ -2,7 +2,9 @@ var fs = require('graceful-fs')
 var h = require('virtual-hyperscript')
 var vdomify = require('vdomify')
 
+var template = fs.readFileSync(__dirname + '/../templates/listen.html', 'utf8')
+
 module.exports = function (state) {
   //return h('.listen', {innerHTML: fs.readFileSync(__dirname + '/../templates/listen.html', 'utf8')})
-  return vdomify('.listen', fs.readFileSync(__dirname + '/../templates/listen.html', 'utf8'))
+  return vdomify('.listen', template)
 }
