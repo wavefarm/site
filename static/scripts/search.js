3f638cdffd7fa8e0b9fa45302ffbacdfc4b19d12
Gabriel Farrell <g@gsf.io>
2015-11-01 06:37:06 -0500

    Semicolons

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,33 +1,33 @@
-(function ($) {
-  var querystring = location.search.substr(1)
-  var query = $.deparam(querystring)
-  $('#q2').val(query.q)
-  $('#date').val(query.date)
-  $('#date2').val(query.date2)
+;(function ($) {
+  var querystring = location.search.substr(1);
+  var query = $.deparam(querystring);
+  $('#q2').val(query.q);
+  $('#date').val(query.date);
+  $('#date2').val(query.date2);
 
-  var template, data
+  var template, data;
 
   function renderResults () {
-    if (!template || !data) return
-    $('#summary').html('<b>' + data.total + '</b> items.')
-    var $results = $('#results')
+    if (!template || !data) return;
+    $('#summary').html('<b>' + data.total + '</b> items.');
+    var $results = $('#results');
     data.hits.forEach(function (hit) {
-      console.log(hit)
-      $results.append(Mustache.render(template, hit))
-    })
+      console.log(hit);
+      $results.append(Mustache.render(template, hit));
+    });
   }
 
   $.get('/templates/search-result.html', function (t) {
-    template = t
-    renderResults()
-  })
+    template = t;
+    renderResults();
+  });
 
   $.ajax({
     url: '/api/search',
     data: querystring,
     success: function (d) {
-      data = d
-      renderResults()
+      data = d;
+      renderResults();
     }
-  })
-})(jQuery)
+  });
+})(jQuery);
