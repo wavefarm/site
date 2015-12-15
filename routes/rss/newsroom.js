8e55366a7874414f448080fdbf108d44513d65d0
ptberry <ptberry@gmail.com>
2015-12-15 10:41:57 -0800

    better API error handling

--- routes/rss/newsroom.js
+++ routes/rss/newsroom.js
@@ -16,11 +16,11 @@ module.exports = function (req, res) {
 	}
 	
 	var dateFormat = 'ddd, DD MMM YYYY H:mm:ss -0500'
-
-	var basePath = 		req.connection.encrypted?'https://':'http://' + req.headers.host;
-		
+	var basePath = 'https://wavefarm.org';
+	if (req.connection && req.headers.host)
+		basePath = 		req.connection.encrypted?'https://':'http://' + req.headers.host;		
 	var sitePath = '';	
-	var feedTitle = 'Wave Farm Newsroom RSS';
+	var feedTitle = 'Newsroom';
 	//var feedUrl =  'https://wavefarm.org/newsroom';
 	var feedUrl =   basePath + req.url;
 	var feedDescription = 'This page combines the posts from both the Wave Farm Newsroom (Transmission Art news, radio news, open calls, media news, radio theatre, and more) and the WGXC Newsroom (Scripts and sounds for WGXC radio hosts to play on the WGXC Morning Show or WGXC Afternoon Show. Breaking news, features, local audio, video, analysis, music, links to events in Greene and Columbia counties, NY.)';
@@ -29,13 +29,13 @@ module.exports = function (req, res) {
 	var feedLastBuildDate = 'Mon, 14 Dec 2015 20:01:51 GMT';
 
 	if (site=='wgxc') {
-		feedTitle = 'WGXC Newsroom RSS';
+		feedTitle = 'WGXC Newsroom';
 		feedDescription = 'Scripts and sounds for WGXC radio hosts to play on the WGXC Morning Show or WGXC Afternoon Show. Breaking news, features, local audio, video, analysis, music, links to events in Greene and Columbia counties, NY.';
 		sitePath =  'wgxc/';
 		feedUrl =  basePath + '/wgxc/newsroom';
 	}
 	else if (site=='ta') {
-		feedTitle = 'Waver Farm Newsroom RSS';
+		feedTitle = 'Waver Farm Newsroom';
 		feedDescription = 'Transmission Art news, radio news, open calls, media news, radio theatre, and more, from Wave Farm.';
 		sitePath =  'ta/';
 		feedUrl =  basePath + '/ta/newsroom';
@@ -45,35 +45,34 @@ module.exports = function (req, res) {
 	if (site=='wgxc') sites = '%20sites:wgxc';
 	else if (site=='ta') sites = '%20sites:transmissionarts';					
 		
-	var params = qs.parse('q=type:news'+sites+'&sort=-date&size=50')	
+	var params = qs.parse('q=type:news'+sites+'&sort=-date&size=40')	
   api.search(params, function (err, results) {
   	
-    if (err) return console.error(err)
-    
     items = [];
-    for (var i = 0; i < results.hits.length; i++) {
-    	    	
-      result = results.hits[i]
-      
-    	if (i==0) {
-    		feedLastBuildDate = moment(result.date).format(dateFormat)
-    		feedPubDate = feedLastBuildDate
-    	}
-      
-      
-      item = {
-      		"title" : result.title,
-      		"description" : result.description,
-      		"link" : basePath + '/' + sitePath + 'newsroom/' + result.id,
-      		"pubDate" : moment(result.date).format(dateFormat),
-      		"guid" : result.id,
-      		"authod" : result.author
-      }            
-      items.push(item)
+    
+    if (err) {
+    	// allow API errors to render page, w/o items, bug log error
+    	console.error(err);
     }
-    
-    
-  	
+    else {
+	    for (var i = 0; i < results.hits.length; i++) {    	    	
+	      result = results.hits[i]      
+	    	if (i==0) {
+	    		feedLastBuildDate = moment(result.date).format(dateFormat)
+	    		feedPubDate = feedLastBuildDate
+	    	}            
+	      item = {
+	      		"title" : result.title,
+	      		"description" : result.description,
+	      		"link" : basePath + '/' + sitePath + 'newsroom/' + result.id,
+	      		"pubDate" : moment(result.date).format(dateFormat),
+	      		"guid" : result.id,
+	      		"authod" : result.author
+	      }            
+	      items.push(item)
+	    }
+    }
+      	
     res.setHeader('Content-Type', 'application/rss+xml; charset=utf-8')
     res.end(mustache.render(res.t['rss/newsroom.xml'], {
       feedTitle: feedTitle,
@@ -83,9 +82,7 @@ module.exports = function (req, res) {
       feedPubDate: feedPubDate,
       feedLastBuildDate: feedLastBuildDate,
       items: items        
-    }))    
-    
-    
+    }))   
   })	
 	
 	
