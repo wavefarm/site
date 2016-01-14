d9de7cae8862e9b62f77a1555359e9849e894028
ptberry <ptberry@gmail.com>
2016-01-14 11:33:14 -0800

    new icons for newsroom rss and show podcasts; newsroom active nav fix

--- static/scripts/wgxc-nav.js
+++ static/scripts/wgxc-nav.js
@@ -2,7 +2,7 @@
   function current (section) { document.getElementById(section+'-nav').style.color = 'white'; }
   if (window.location.pathname == '/wgxc/about') current('about');
   else if (window.location.pathname.indexOf('/wgxc/schedule')==0) current('schedule');
-  else if (window.location.pathname == '/wgxc/newsroom') current('newsroom');
+  else if (window.location.pathname.indexOf('/wgxc/newsroom')==0) current('newsroom');
   else if (window.location.pathname == '/wgxc/volunteer') current('volunteer');
   else if (window.location.pathname.indexOf('/wgxc/calendar')==0) current('calendar');
   else if (window.location.pathname == '/wgxc/donate') current('support');  
