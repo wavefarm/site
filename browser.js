7a2bc5ace5f6c1e4e89660161092f0bd1f93d3e9
Gabriel Farrell <g@gsf.io>
2016-01-15 07:47:35 -0500

    No more state in the browser

--- browser.js
+++ browser.js
@@ -1,29 +1,10 @@
-var o = require('observ')
-var struct = require('observ-struct')
-
-
 var env = process.env.NODE_ENV;
 
 // SSE reload on dev
 if (env == 'dev') require('deva');
 
-// THE STATE
-window.state = struct({
-  archive: struct({
-    item: o(),
-    params: o(),
-    results: o()
-  }),
-  section: o(),
-  schemas: o(),
-  scrollX: o(),
-  scrollY: o(),
-  title: o()
-})
-
 require('./lib/pointer')()
 require('./lib/schedule')()
-require('./lib/schemas')()
 require('./lib/subnav')()
 require('./lib/tweets')()
 require('./lib/listenlive')()
